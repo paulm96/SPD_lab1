467d0717a5d324f519b0a0a5186748c9757c8e70
pawel <pawi_xu@interia.pl>
2018-03-03 19:30:15 +0100

    added permutations

--- main.cpp
+++ main.cpp
@@ -20,10 +20,28 @@ int Cmax(int **array, int tasks, int mach) {
     return machines[2];
 }
 
+void perm(int &min, int **result_array, int **array, const int &tasks, const int &machines, int k) {
+    if (1 == k) {
+        int value = Cmax(array, tasks, machines);
+        if (value < min) {
+            min = value;
+            //std::cout << tasks << " " << machines;
+            //std::copy(&array[0][0], &array[0][0] + tasks * machines, &result_array[0][0]);
+        }
+
+    } else {
+        for (int i = 0; i < k; ++i) {
+            std::swap(array[i], array[k]);
+            perm(min, result_array, array, tasks, machines, k - 1);
+            std::swap(array[i], array[k]);
+        }
+    }
+}
+
 
 int main() {
     std::ifstream in;
-    in.open("./NEH1.DAT", std::ios::in);
+    in.open("./NEH3.DAT", std::ios::in);
     if (in.good())
         std::cout << "Opened succesfully\n";
 
@@ -37,12 +55,16 @@ int main() {
     ss >> machines;
 
 
-    int **array = new int *[tasks];
+    int **array = new int *[tasks];    //main array
     for (auto i = 0; i < tasks; ++i) {
         array[i] = new int[machines];
     }
+    int **result_array = new int *[tasks];  //array for the result
+    for (auto i = 0; i < tasks; ++i) {
+        result_array[i] = new int[machines];
+    }
 
-    for (int i = 0; i < tasks ; ++i) {
+    for (int i = 0; i < tasks; ++i) {   //to fill the main array
         std::getline(in, s);
         std::istringstream converter(s);
         for (int j = 0; j < machines; ++j) {
@@ -50,10 +72,18 @@ int main() {
         }
     }
 
-    auto result = Cmax(array, tasks, machines);
-    std::cout << result;
 
-    getchar();
+    int result = std::numeric_limits<int>::max();   //max_int
+    perm(result, result_array, array, tasks, machines, tasks - 1);  //all permutations
+
+    std::cout << "Result: " <<  result << std::endl;
+//    for (int i = 0; i < tasks; ++i) {
+//        for (int j = 0; j < machines; ++j) {
+//            std::cout << result_array[i][j] << " ";
+//        }
+//        std::cout << std::endl;
+//    }
+
 
     return 0;
 }
